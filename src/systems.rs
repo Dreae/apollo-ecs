7efe2b0dd52cb8acb22ce56545918a31070f1f72
Dreae <dreae@dreae.onl>
2017-12-02 15:53:22 -0500

    Add some docs

--- src/systems.rs
+++ src/systems.rs
@@ -1,7 +1,14 @@
 use super::{EntityEditor, EntityQuery};
 
 pub trait IterativeSystem {
+    /// Static method to return the query that will be used
+    /// to filter the world's entities before they are passed
+    /// to this system
     fn get_query() -> EntityQuery where Self: Sized;
+
     // TODO: Shound't take an EntityEditor
+    /// The main loop for this system, `process` is called
+    /// for every entity that matches this system's query
+    /// on every world tick.
     fn process(&mut self, ent: EntityEditor);
 }
--- src/world.rs
+++ src/world.rs
@@ -10,10 +10,12 @@ pub struct World {
 }
 
 impl World {
+    /// Create a new ECS world with a default capacity for entities of 131072
     pub fn new() -> World {
-        World::with_capacity(524288)
+        World::with_capacity(131072)
     }
 
+    /// Create a new world with custom initial capacity specified
     pub fn with_capacity(capacity: usize) -> World {
         World {
             entities: Vec::with_capacity(capacity),
@@ -21,16 +23,55 @@ impl World {
         }
     }
 
+    /// Registers a new iterative system, which will be called for every entity that
+    /// matches its query on every tick.
+    /// 
+    /// # Examples
+    /// ```
+    /// use apollo_ecs::*;
+    /// use apollo_ecs::systems::IterativeSystem;
+    /// 
+    /// struct SimpleSystem;
+    /// 
+    /// struct Phys {
+    ///     mass: f32
+    /// }
+    /// struct Disabled;
+    /// 
+    /// impl SimpleSystem {
+    ///     fn new() -> SimpleSystem {
+    ///         SimpleSystem
+    ///     }
+    /// }
+    /// 
+    /// impl IterativeSystem for SimpleSystem {
+    ///     fn get_query() -> EntityQuery {
+    ///         EntityQuery::new(Matchers::with::<Phys>().without::<Disabled>())
+    ///     }
+    /// 
+    ///     fn process(&mut self, ent: EntityEditor) {
+    ///         let phys = ent.get::<Phys>().unwrap();
+    ///         // Do something with phys here.
+    ///     }
+    /// }
+    /// 
+    /// let mut world = World::new();
+    /// world.register_iterative_system(SimpleSystem::new());
+    /// let ent = world.create_entity();
+    /// world.edit(ent).unwrap().add(Phys { mass: 100.0 });
+    /// ```
     pub fn register_iterative_system<T>(&mut self, system: T) where T: IterativeSystem + 'static {
         self.iterative_systems.push((Box::new(system), T::get_query()));
     } 
 
+    /// Allocates space for a new entity and returns its ID
     pub fn create_entity(&mut self) -> Entity {
         self.entities.push(Vec::with_capacity(12));
 
         self.entities.len() - 1
     }
 
+    /// Removes an entity from the world and cleans up its components
     pub fn drop_entity(&mut self, ent: Entity) {
         if ent < self.entities.len() {
             let components = self.entities.remove(ent);
@@ -43,6 +84,7 @@ impl World {
         }
     }
 
+    /// Edit an entity `ent`, if it exists.
     pub fn edit(&mut self, ent: Entity) -> Option<EntityEditor> {
         if let Some(components) = self.entities.get_mut(ent) {
             Some(EntityEditor::new(ent, components))
@@ -51,10 +93,13 @@ impl World {
         }
     }
 
+    /// Filter all entities that match the provided query, and return an iterator
+    /// to them.
     pub fn filter_entities<'a, 'q>(&'a mut self, query: &'q Query) -> QueryRunner<'q> {
         QueryRunner::new(self.entities.as_ptr(), self.entities.len(), query)
     }
 
+    /// The main loop for a world. Calling `process` runs all ready systems in this world.
     pub fn process(&mut self) {
         for sys in self.iterative_systems.iter_mut() {
             for ent in QueryRunner::new(self.entities.as_ptr(), self.entities.len(), &sys.1) {
